32c48a1d2a50e05b4ba795ec3db0396e365f0b3a
Katarzyna-Stulgis <kasia2011r@gmail.com>
2022-05-17 12:16:00 +0200

    Dodaj pliki projektów.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using zad.Models;
+
+namespace zad.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<zad.Models.User> User { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace zad.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string surname { get; set; }
+    }
+}
